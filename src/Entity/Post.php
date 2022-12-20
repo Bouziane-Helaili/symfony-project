c9a975abeeff92d7a3e5e3408557593841c12d58
Bouziane-Helaili <adressebouziane@hotmail.com>
2022-12-20 18:46:03 +0100

    edit type of  in Post.php

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\PostRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
@@ -18,7 +19,7 @@ class Post
     #[ORM\Column(length: 255)]
     private ?string $title = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: Types::TEXT)]
     private ?string $content = null;
 
     #[ORM\Column]
