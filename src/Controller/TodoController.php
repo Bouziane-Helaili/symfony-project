b5a2d616f7ee672a61e1dbc208d2a5c3e461bebe
Bouziane-Helaili <adressebouziane@hotmail.com>
2022-12-27 17:15:04 +0100

    add function edit, delete and reset session

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -9,6 +9,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends AbstractController
 {
+
+    //listing des tâches
     #[Route('/todo', name: 'todo')]
     public function index(Request $request): Response
     {
@@ -22,13 +24,14 @@ class TodoController extends AbstractController
                 "correction" => "corriger mes examens"
             ];
             $session->set("todo", $todo);
-            $this->addFlash("info","Enregistrement bien éffectué");
+            $this->addFlash("info", "Enregistrement bien effectué");
         }
         // sinon je l'affiche
         return $this->render('todo/index.html.twig');
     }
-    
-    #[Route('/todo/add/{name}/{content}', name: 'todo.add')]
+
+    //Ajout d'une tâche
+    #[Route('/todo/add/{name}', name: 'todo.add')]
     public function addTodo(Request $request, $name, $content): Response
     {
         $session = $request->getSession();
@@ -36,23 +39,85 @@ class TodoController extends AbstractController
         if ($session->has('todo')) {
             //si oui
             //vérifier que le todo n'est pas le même que celui de la session
-            $todo=$session->get('todo');
+            $todo = $session->get('todo');
             if (isset($todo[$name])) {
                 //Si oui, afficher erreur
-                $this->addFlash("error","La tâche $name existe déjà dans la liste");
-            }else{
+                $this->addFlash("error", "La tâche $name existe déjà dans la liste");
+            } else {
                 //si non, le nouveau todo est enregistré et afficher un message de succès
-                $todo[$name]=$content;
-                $this->addFlash("success","Enregistrement de la tâche $name a bien été effectuée");
+                $todo[$name] = $content;
                 $session->set('todo', $todo);
+                $this->addFlash("success", "Enregistrement de la tâche $name a bien été effectuée");
             }
-            
         } else {
             //si non
             //Afficher une erreur et rediriger vers le controller index
-            $this->addFlash("error","la liste n'est pas encore initialisée");
+            $this->addFlash("error", "la liste n'est pas encore initialisée");
         }
+        return $this->redirectToRoute("todo");
+    }
 
+    //Modification d'une tâche
+    #[Route('/todo/update/{name}/{content}', name: 'todo.update')]
+    public function updateTodo(Request $request, $name, $content): Response
+    {
+        $session = $request->getSession();
+        // Vérifier si j'ai mon tableau todo dans la session
+        if ($session->has('todo')) {
+            //si oui
+            //vérifier que le todo n'est pas le même que celui de la session
+            $todo = $session->get('todo');
+            if (!isset($todo[$name])) {
+                //Si oui, afficher erreur
+                $this->addFlash("error", "La tâche $name n'existe pas dans la liste");
+            } else {
+                //si non, le nouveau todo est enregistré et afficher un message de succès
+                $todo[$name] = $content;
+                $session->set('todo', $todo);
+                $this->addFlash("success", "La modification de la tâche $name a bien été effectuée");
+            }
+        } else {
+            //si non
+            //Afficher une erreur et rediriger vers le controller index
+            $this->addFlash("error", "la liste n'est pas encore initialisée");
+        }
+        return $this->redirectToRoute("todo");
+    }
+
+
+    //Suppression d'une tâche
+    #[Route('/todo/delete/{name}', name: 'todo.delete')]
+    public function deleteTodo(Request $request, $name): Response
+    {
+        $session = $request->getSession();
+        // Vérifier si j'ai mon tableau todo dans la session
+        if ($session->has('todo')) {
+            //si oui
+            //vérifier que le todo n'est pas le même que celui de la session
+            $todo = $session->get('todo');
+            if (!isset($todo[$name])) {
+                //Si oui, afficher erreur
+                $this->addFlash("error", "La tâche $name n'existe pas dans la liste");
+            } else {
+                //si non, le nouveau todo est enregistré et afficher un message de succès
+                unset($todo[$name]);
+                $session->set('todo', $todo);
+                $this->addFlash("success", "La suppression de la tâche $name a bien été effectuée");
+            }
+        } else {
+            //si non
+            //Afficher une erreur et rediriger vers le controller index
+            $this->addFlash("error", "la liste n'est pas encore initialisée");
+        }
+        return $this->redirectToRoute("todo");
+    }
+
+    #[Route('/todo/reset', name: 'todo.reset')]
+    public function resetTodo(Request $request): Response
+    {
+        $session = $request->getSession();
+        $session->remove('todo');
+        $this->addFlash("infor", "la session a été réinitialisée");
         return $this->redirectToRoute("todo");
     }
 }
